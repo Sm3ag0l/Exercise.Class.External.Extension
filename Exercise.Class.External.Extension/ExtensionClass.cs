fbe86d30233de1c6e2882cd07c3c240f189ea053
Thomas Schildhauer <Thomas.Schildhauer@outlook.com>
2019-04-18 20:05:39 +0200

    setup project and solution

---
+++ Exercise.Class.External.Extension/ExtensionClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Class.External.Extension
+{
+    public static class ExtensionClass
+    {
+        public static void StringExtension(this TestClass text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Exercise.Class.External.Extension/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Class.External.Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+
+            testClass.StringExtension();
+
+            Console.ReadKey();
+        }
+    }
+}
